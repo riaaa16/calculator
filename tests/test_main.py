bbe8acef4f4051715f0a362672d37713b17664ab
Ria <vg435@njit.edu>
2024-09-17 16:48:00 -0400

    added subtraction

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,10 @@
 '''My Calculator Test'''
-from app.main import addition
+from app.main import addition, subtraction
 
 def test_addition():
     '''Addition function'''
-    assert addition(1,1)  == 2
+    assert addition(1,1) == 2
+
+def test_subtraction():
+    '''Subtraction function'''
+    assert subtraction(1,1) == 0
